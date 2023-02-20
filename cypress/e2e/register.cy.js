b365159eafe21bc98baf7812d746c12d1accd318
milanzivanovns <milan.zivanov.ns@gmail.com>
2023-02-20 13:44:24 +0100

    Added Register POM and register js test

---
+++ cypress/e2e/register.cy.js
@@ -0,0 +1,29 @@
+/// <reference types="Cypress" />
+
+import { registerPage } from "../../page_objects/registerPOM";
+import { faker } from "@faker-js/faker";
+
+const credetials = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.lorem.word(8) + 1
+    
+    }
+
+describe("Register page test", () => {
+    before("visit app and click the register link", () => {
+        cy.visit("/");
+        registerPage.registerLink.click();
+        cy.url().should("contain", "/register");
+    })
+
+    it("Valid user registration", () => {
+        registerPage.registerUserWithValidData(
+            credetials.firstName, 
+            credetials.lastName, 
+            credetials.email, 
+            credetials.password, 
+            credetials.password);
+    })
+})
